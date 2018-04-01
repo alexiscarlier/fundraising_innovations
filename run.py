4a8ea09de58ea593eee11424464bcc95602bba9c
Alexis Carlier <alexis.carlier0@gmail.com>
2018-04-01 17:34:56 +0100

    Update run file to create start page when run

--- run.py
+++ run.py
@@ -1,17 +1,82 @@
-tools_page_link = "start"
-tools_page = wiki.pages.get(tools_page_link)
-# define the header of the tools table
-tools_header = "\n^ Tool name ^ Category ^ Description ^ Theories ^\n"
-# create the new page content
-new_tools_page = format_table(tools_page, tools_header, tools_records, 'Toolname', tool_row_constructor)
-# publish it to DW
-wiki.pages.set(tools_page_link, new_tools_page)
+import dokuwiki
+import airtable as at
+import os
+from functools import reduce
+import string
+import re
+import getpass
+
+# muhkejwew31%
+# keyklyto2SYSfTty6
+
+# THE CLASSES I'M IMPORTING
+
+import table_formatter
+
+# THIS RUNS TO STORE PASSWORD AND USERKEY AS ENV VARIABLES
+
+pss = getpass.getpass("Please enter your dokuwiki password: ")
+os.environ['DOKUWIKI_PASS'] = pss
+userkey = input("Please enter your airtable user key: ")
+os.environ['AIRTABLE_API_KEY'] = userkey
+
+# THIS IS ALL SET UP
+
+# define a punctuation stripper for using later in pagename constructors
+punctuation_translator = str.maketrans('', '', string.punctuation)
+
+"""
+Set up the APIs
+"""
+
+# for DokuWiki we need url, user name and password
+usr = "alexiscarlier"  # this has to be added to the remoteuser in config:authentication
+
+# the official DW
+url = "http://localhost/~alexiscarlier/dokuwiki"
+pss = os.environ['DOKUWIKI_PASS']
+
+# for Airtable we need database key, user key and names of the requested tables
+basekey_giving_researchers_shared = 'appBzOSifwBqSuVfH'
+basekey_giving_impact = "apprleNrkR7dTtW60"
+
+userkey = os.environ['AIRTABLE_API_KEY']
+
+tools_sample = 'tools_public_sample'
+theories = 'Theories'
+papers = 'papers_mass'
+ftse = "ftse100+givingpolicies"
+experiments = "Charity experiments"
 
 
-# Relies on the format_table, and tool_row_constructor
+"""
+Fetch the relevant tables from Airtable
+"""
+
+tools_table = at.Airtable(basekey_giving_researchers_shared, tools_sample, api_key=userkey)
+tools_records = tools_table.get_all()
+
+theories_table = at.Airtable(basekey_giving_researchers_shared, theories, api_key=userkey)
+theories_records = theories_table.get_all(fields=['Theory'])
+
+papers_table = at.Airtable(basekey_giving_researchers_shared, papers, api_key=userkey)
+papers_records = papers_table.get_all(fields=['Authors', 'Title', 'Journal', 'Year', 'Pages', 'URL'])
+
+ftse_table = at.Airtable(basekey_giving_impact, ftse, api_key=userkey)
+ftse_records = ftse_table.get_all()
+
+experiments_table = at.Airtable(basekey_giving_researchers_shared, experiments, api_key=userkey)
+experiments_records = experiments_table.get_all()
 
 
+"""
+Initialize the wiki object
+"""
 
+wiki = dokuwiki.DokuWiki(url, usr, pss)
+
+
+# STAND ALONE FUNCTION TO BE ENCAPSULATED LATER
 
 def tool_row_constructor(record):
     """
@@ -34,3 +99,21 @@ def tool_row_constructor(record):
     row = "| " + tool_page_name + " | " + category[0] + " | " +\
         findings[0].rstrip() + " | " + ', '.join(theory_names) + " |\n"
     return row
+
+
+# THIS IS THE CODE THAT RUNS AND DOES SOMETHING
+
+tableformatter = table_formatter.TableFormatter()
+
+tools_page_link = "start"
+tools_page = wiki.pages.get(tools_page_link)
+# define the header of the tools table
+tools_header = "\n^ Tool name ^ Category ^ Description ^ Theories ^\n"
+# create the new page content
+tableformatter.format_table(tools_page, tools_header, tools_records, 'Toolname', tool_row_constructor)
+new_tools_page = tableformatter.new_page
+# publish it to DW
+wiki.pages.set(tools_page_link, new_tools_page)
+
+
+# Relies on the format_table, and tool_row_constructor
